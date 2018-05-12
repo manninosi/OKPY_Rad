9fd4eac069775d0c94f9cadbec05b1701dbe6495
manninosi <manninom@oregonstate.edu>
2018-05-12 09:32:09 -0700

    Created code outlines of functions for McaHisto() class.

--- okpy_rad/osu_rad_package/histo.py
+++ okpy_rad/osu_rad_package/histo.py
@@ -11,25 +11,34 @@ class McaHisto(RadDevice):
         self.xem.SetWiteInValue(self.run_mode, 2**32-1)
         self.xem.UpdateWireIns()
 
-    def run_mca(self, time, time_sel = 1, plot = 0):
+    def run_mca(self, time, time_sel = 0, plot = 0):
         """Method to run MCA for FPGA and acquire all data
         """
         MCA_done = 0
-        if self.run_mode = 2:
-            if time_sel = 1: #real time_sel
+        if self.run_mode == 2:
+            if time_sel == 1: #real time_sel
                 return None
-                if plot = 1:
+                if plot == 1:
+                    pass
                     #Print the data as it streams in
             else: #Live Time
-                if plot = 1:
+                pass
+                if plot == 1:
+                    pass
                     #Print the data as it streams in
                 return None
+        else:
+            print "Run Mode is not set to 2 for this mode"#MCA run mode is 2
+            #Consider adding function to change mode
     def plot_mca(self):
         pass
+
     def find_peaks(self):
-        if self.data_acquired = 0:
+        if self.data_acquired == 0:
             print "Acquire MCA data before finding peaks"
         else:
+            pass
             #Find peaks
     def get_calibration(self):
+        pass
         #Use peak finding to generate calibration
